b6232e2c9756d5b907140d2c7b888d0c6f6e701d
Selim Kac <selim.kacer@outscale.com>
2019-06-18 13:43:01 +0000

    Connector to be initialized with access key and secret key

--- osc_python_sdk/call.py
+++ osc_python_sdk/call.py
@@ -7,14 +7,17 @@ import json
 
 class Call(object):
     def __init__(self, **kwargs):
-        self.profile = kwargs.pop('profile', 'default')
+        self.credentials = {'access_key': kwargs.pop('access_key', None),
+                            'secret_key': kwargs.pop('secret_key', None),
+                            'region': kwargs.pop('region', None),
+                            'profile': kwargs.pop('profile', 'default')}
         self.version = kwargs.pop('version', 'latest')
         self.host = kwargs.pop('host', None)
         self.ssl = kwargs.pop('_ssl', True)
 
     def api(self, action, **data):
         try:
-            credentials = Credentials(self.profile)
+            credentials = Credentials(**self.credentials)
             host = (self.host if self.host
                     else 'api.{}.outscale.{}'.format(credentials.get_region(), credentials.get_url_extension()))
             uri = '/api/{}/{}'.format(self.version, action)
