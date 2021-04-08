a256551a5aa959c1f817a8db9efd8f95627587f0
Matthias Gatto <matthias.gatto@outscale.com>
2021-04-08 15:40:04 +0200

    use Outscale Standard tool configuration
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- osc_sdk_python/credentials.py
+++ osc_sdk_python/credentials.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+ORIGINAL_PATH = os.path.join(os.path.expanduser('~'),'.oapi_credentials')
+STD_PATH = os.path.join(os.path.expanduser('~'),'.osc/config.json')
 
 class Credentials:
     def __init__(self, profile=None, access_key=None, secret_key=None, region=None):
@@ -18,28 +20,35 @@ class Credentials:
         self.region = os.getenv('OSC_REGION', 'us-west-1')
         return self.access_key and self.secret_key
 
+    def load_credentials_from_file_(self, profile, f):
+        try:
+            credentials = json.load(f)
+            if not profile in credentials:
+                self.access_key = ''
+                self.secret_key = ''
+                self.region = 'eu-west-2'
+            else:
+                self.access_key = credentials.get(profile).get('access_key', '')
+                self.secret_key = credentials.get(profile).get('secret_key', '')
+                self.region = credentials.get(profile).get('region', 'us-west-1')
+        except ValueError:
+            print ('Decoding json of "~/.oapi_credentials" has failed.')
+            raise
+        except AttributeError as e:
+            print ('{}'.format(e))
+            raise
+
     def load_credentials_from_file(self, profile):
         try:
-            with open(os.path.join(os.path.expanduser('~'),'.oapi_credentials')) as f:
-                try:
-                    credentials = json.load(f)
-                    if not profile in credentials:
-                        self.access_key = ''
-                        self.secret_key = ''
-                        self.region = 'eu-west-2'
-                    else:
-                        self.access_key = credentials.get(profile).get('access_key', '')
-                        self.secret_key = credentials.get(profile).get('secret_key', '')
-                        self.region = credentials.get(profile).get('region', 'us-west-1')
-                except ValueError:
-                    print ('Decoding json of "~/.oapi_credentials" has failed.')
-                    raise
-                except AttributeError as e:
-                    print ('{}'.format(e))
-                    raise
+            with open(STD_PATH) as f:
+                self.load_credentials_from_file_(profile, f)
         except IOError:
-            print ('"~/.oapi_credentials" not found.')
-            raise
+            try:
+                with open(ORIGINAL_PATH) as f:
+                    self.load_credentials_from_file_(profile, f)
+            except IOError:
+                print ('nor "', STD_PATH, '" nor "' ,ORIGINAL_PATH,'" found.')
+                raise
 
     def get_region(self):
         return self.region
