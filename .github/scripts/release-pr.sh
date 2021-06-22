95597eb25156b9134b3559f62434a66df1eb3364
Jérôme Jutteau <jerome.jutteau@outscale.com>
2021-06-22 10:48:31 +0200

    ci: auto-build and push release on new osc-api version
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

---
+++ .github/scripts/release-pr.sh
@@ -0,0 +1,34 @@
+#!/bin/env bash
+set -e
+osc_api_version=$1
+
+if [ -z "$osc_api_version" ]; then
+    echo "run $0 with version tag as argument, abort."
+    exit 1
+fi
+
+if [ -z "$GH_TOKEN" ]; then
+    echo "GH_TOKEN is missing, abort."
+    exit 1
+fi
+
+root=$(cd "$(dirname $0)/../.." && pwd)
+
+if [ ! -e "$root/sdk_version" ]; then
+    echo "$root/sdk_version not found, abort."
+    exit 1
+fi
+
+new_sdk_version=$(cat $root/sdk_version)
+major=$(echo $new_sdk_version | cut -d '.' -f 1)
+branch_name="autobuild-$new_sdk_version"
+
+# https://docs.github.com/en/free-pro-team@latest/rest/reference/pulls#create-a-pull-request
+result=$(curl -s -X POST -H "Authorization: token $GH_TOKEN" -d "{\"head\":\"$branch_name\",\"base\":\"master\",\"title\":\"SDK v$new_sdk_version\",\"body\":\"Automatic build of SDK v$new_sdk_version version based on Outscale API v$osc_api_version\"}" "https://api.github.com/repos/outscale/osc-sdk-python/pulls")
+
+errors=$(echo $result | jq .errors)
+
+if [ "$errors" != "null" ]; then
+    echo "errors while creating pull request, abort."
+    exit 1
+fi
