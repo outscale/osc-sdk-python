ad3c8576e46bfdbb09bc8056161fafe09ecdceeb
Jérôme Jutteau <jerome.jutteau@outscale.com>
2021-08-17 17:06:07 +0200

    Move bandit conf to project's root
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- scripts/test_bandit.sh
+++ scripts/test_bandit.sh
@@ -5,6 +5,6 @@ echo -n "$(basename $0)"
 PROJECT_ROOT=$(cd "$(dirname $0)/.." && pwd)
 cd $PROJECT_ROOT
 . .venv/bin/activate
-bandit -c $PROJECT_ROOT/tests/bandit.conf -r osc_sdk_python
+bandit -c $PROJECT_ROOT/bandit.conf -r osc_sdk_python
 
 echo "OK"
