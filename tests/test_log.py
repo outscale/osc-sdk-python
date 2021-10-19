ec51016ae668cd285d9ff79c92723ac940ffbad1
Matthias Gatto <matthias.gatto@outscale.com>
2021-10-19 14:46:21 +0200

    log: add test
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ tests/test_log.py
@@ -0,0 +1,32 @@
+import unittest
+import sys
+sys.path.append("..")
+from osc_sdk_python import *
+
+class TestLog(unittest.TestCase):
+
+    def test_listing(self):
+        gw = Gateway()
+        gw.log.config(type=LOG_MEMORY, what=LOG_KEEP_ONLY_LAST_REQ)
+        gw.ReadVms()
+        self.assertEqual(gw.log.str(),
+                         """uri: /api/latest/ReadVms
+payload:
+{}"""
+                         )
+
+        gw.ReadVms(Filters={'TagKeys': ['test']})
+        self.assertEqual(gw.log.str(),
+                         """uri: /api/latest/ReadVms
+payload:
+{
+  "Filters": {
+    "TagKeys": [
+      "test"
+    ]
+  }
+}"""
+                         )
+
+if __name__ == '__main__':
+    unittest.main()
