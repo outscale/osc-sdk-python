790eb94bcd75ec23e859e0849decfde1f364da60
Jérôme Jutteau <jerome.jutteau@outscale.com>
2022-10-19 09:57:20 +0200

    Fix login password auth
    Password auth was only working when OSC_ACCESS_KEY and OSC_SECRET_KEY env variables were set.
    
    fixes #85
    
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- tests/test_password.py
+++ tests/test_password.py
@@ -7,6 +7,8 @@ from osc_sdk_python import Gateway
 class TestLoginPassword(unittest.TestCase):
 
     def test_login(self):
+        os.environ.pop("OSC_ACCESS_KEY", None)
+        os.environ.pop("OSC_SECRET_KEY", None)
         email = os.getenv('OSC_TEST_LOGIN')
         password = os.getenv('OSC_TEST_PASSWORD')
         self.assertNotEqual(email, None)
