758d0a18e37112bcb21ccbf7daa9028518adf73d
Matthias Gatto <matthias.gatto@outscale.com>
2024-05-14 16:19:35 +0200

    better exeptions for 500 and 400
    fix #130
    
    Signed-off-by: Mikhaël LOLLO <mikhael.lollo@3ds.com>
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- osc_sdk_python/requester.py
+++ osc_sdk_python/requester.py
@@ -1,5 +1,7 @@
+from requests import Session, HTTPError
 from requests import Session
 from requests.adapters import HTTPAdapter
+from requests.exceptions import JSONDecodeError
 from urllib3.util.retry import Retry
 
 class Requester:
@@ -23,5 +25,82 @@ class Requester:
             session.mount("https://", self.adapter)
             session.mount("http://", self.adapter)
             response = session.post(self.endpoint, data=payload, headers=headers, verify=True,)
-            response.raise_for_status()
+            self.raise_for_status(response)
             return response.json()
+
+    def raise_for_status(self, response):
+        http_error_msg = ""
+        error_code = None
+        request_id = None
+        reason = self.get_default_reason(response)
+        code_type = None
+
+        try:
+            error = response.json()
+        except JSONDecodeError:
+            pass
+        else:
+            if "__type" in error:
+                error_code = error.get("__type")
+                reason = error.get("message")
+                request_id = response.headers.get("x-amz-requestid")
+            else:
+                request_id = (error.get("ResponseContext") or {}).get("RequestId")
+                errors = error.get("Errors")
+                if errors:
+                    error = errors[0]
+                    error_code = error.get("Code")
+                    reason = error.get("Type")
+                    if error.get("Details"):
+                        code_type = reason
+                        reason = error.get("Details")
+                    else:
+                        code_type = None
+
+            if 400 <= response.status_code < 500:
+                if error_code and request_id:
+                    http_error_msg = (
+                        f"Client Error --> status = {response.status_code}, "
+                        f"code = {error_code}, "
+                        f'{"code_type = " if code_type is not None else ""}'
+                        f'{code_type + ", " if code_type is not None else ""}'
+                        f"Reason = {reason}, "
+                        f"request_id = {request_id}, "
+                        f"url = {response.url}"
+                    )
+                else:
+                    http_error_msg = (
+                        f"{response.status_code} Client Error: {reason} for url: {response.url}"
+                    )
+
+            elif 500 <= response.status_code < 600:
+                if error_code and request_id:
+                    http_error_msg = (
+                        f"Server Error --> status = {response.status_code}, "
+                        f"code = {error_code}, "
+                        f'{"code_type = " if code_type is not None else ""}'
+                        f'{code_type + ", " if code_type is not None else ""}'
+                        f"Reason = {reason}, "
+                        f"request_id = {request_id}, "
+                        f"url = {response.url}"
+                    )
+                else:
+                    http_error_msg = (
+                        f"{response.status_code} Server Error: {reason} for url: {response.url}"
+                    )
+
+            if http_error_msg:
+                raise HTTPError(http_error_msg, response=response)
+
+    def get_default_reason(self, response):
+        if isinstance(response.reason, bytes):
+            # We attempt to decode utf-8 first because some servers
+            # choose to localize their reason strings. If the string
+            # isn't utf-8, we fall back to iso-8859-1 for all other
+            # encodings. (See PR #3538)
+            try:
+                return response.reason.decode("utf-8")
+            except UnicodeDecodeError:
+                return response.reason.decode("iso-8859-1")
+        else:
+            return response.reason
